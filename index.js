7a0f5f0cd0e13b518aded97d426ac089d69c1efe
ImCheeini <59221034+Nicat-dcw@users.noreply.github.com>
2022-06-28 23:22:11 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 const { Client, MessageButton, MessageEmbed, MessageMenu, MessageActionRow, MessageSelectMenu } = require("discord.js")
 
+.setDescription(`
+*Your Note Successfully Added!*
+
+
+
+
+
+
+Details;
+• Note Name: ${args[0]}
+• Note Content: 
+\`\`\`md
+${args[1]}
+\`\`\`
+`)
+        .setFooter("By Cheeini | This Message Avaliable 10 Seconds.")
+       message.channel.send({embeds:[added], components: [row, row2], ephemeral: true}).then(x => {
+             setTimeout(function(){
+            
+                 
+            
+             
+           x.delete()
+             },10000)
+            })
+    }
+  }
+    if(command === "list"){
+       const QuickBoard = require('quickboard');
+const board = new QuickBoard({
+  max: 10,
+ data: client.guilds.cache.get(message.guild.id).members.cache.map(member => ({ value: db.fetch(`notes_${member.user.id}`), member })), // client = discord.js client, database = quick.db
+  map: (item, index) => `${index + 1}. ${item.member.displayName} - ${item.note} `,
+  sort: (according, current) => current.value - according.note,
+}).create();
+    
+
+
+        message.reply(`
+             ${board || null ? `Note Name: ${db.get(`notes_`)} | Owner: You. `: "ejejjs"}`)
+            
+                     
+               //getLeaderboard (1, 5)
+        
+    
+        
+    
+    }
+    
+  
 })
 client.on('interactionCreate', interaction => {
 	if (!interaction.isSelectMenu()) return;
@@ -22,6 +72,7 @@ client.on('interactionCreate', interaction => {
 	  	    .setStyle('LONG') //IMPORTANT: Text Input Component Style can be 'SHORT' or 'LONG'
 	  	    .setPlaceholder(fetch.note)
 	  	    .setRequired(true) // If it's required or not,
+            
 	  	    
 	  	  )
 	  	  showModal(modal, {
@@ -29,10 +80,12 @@ client.on('interactionCreate', interaction => {
       interaction: interaction // Show the modal with interaction data.
     });
       }
+    
 	
 	if(interaction.values[0] === "delete_note"){
 	  db.delete(`notes_${interaction.user.id}`)
         const msgId = db.fetch(`notesMsg_${interaction.user.id}`)
+        
       
       return interaction.reply("**Your Note was Deleted!**")
 	}
@@ -60,3 +113,4 @@ client.on("interactionCreate", (interaction) => {
     }
 })
 client.login("")
+
